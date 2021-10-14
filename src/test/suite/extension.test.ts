296d556dcd72d2d574a5bfa6d8622b2a09e51309
Danny Tuppeny <danny@tuppeny.com>
2021-10-14 14:30:16 +0100

    Print cwd in test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -9,6 +9,7 @@ suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
 	test('Sample test', () => {
+		console.log(process.cwd());
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
